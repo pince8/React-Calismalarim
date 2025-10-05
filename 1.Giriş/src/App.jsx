f095ab7eb09f197e0b23ffc2f6ede3feb3af4d82
Pinar Celik <pcelik157@gmail.com>
2025-10-05 14:57:14 +0300

    giriş eklendi

---
+++ 1.Giriş/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //Javascaript kodları 
+
+  return (
+    //html kodları 
+    <div
+      style={{ backgroundColor: 'pink', color: 'black' }}>
+      pinar
+
+    </div>
+  )
+}
+
+export default App
