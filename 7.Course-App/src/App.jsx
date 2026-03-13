44af68b75bf49bdbe1de6866b249b3a286950887
pinarcelik <pcelik157@gmail.com>
2026-03-13 17:42:30 +0300

    yeni compenentler oluşturuldu ekranda kurslar listelendi

--- 7.Course-App/src/App.jsx
+++ 7.Course-App/src/App.jsx
@@ -4,12 +4,20 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Header from './Header'
+import { courses } from './Data'
+import Course from './Course'
+
 
 function App() {
 
   return (
     <div>
       <Header />
+      {
+        courses?.map((course) => (
+          <Course key={course.id} course={course} />
+        ))
+      }
     </div>
   )
 }
---
+++ 7.Course-App/src/Data.jsx
@@ -0,0 +1,53 @@
+import Kurs1 from "./images/kurs1.jpg";
+import Kurs2 from "./images/kurs2.jpg";
+import Kurs3 from "./images/kurs3.jpg";
+import Kurs4 from "./images/kurs4.jpg";
+
+
+export const courses = [
+    {
+        id: 1,
+        title: "React",
+        description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+        price: 200,
+        link: " https://open.spotify.com/intl-tr",
+        image: Kurs1
+
+    },
+    {
+        id: 2,
+        title: "React",
+        description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+        price: 200,
+        link: "https://open.spotify.com/intl-tr",
+        image: Kurs1
+
+    },
+    {
+        id: 3,
+        title: "React",
+        description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+        price: 200,
+        link: "https://open.spotify.com/intl-tr",
+        image: Kurs2
+
+    },
+    {
+        id: 4,
+        title: "React",
+        description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+        price: 200,
+        link: "https://open.spotify.com/intl-tr",
+        image: Kurs3
+
+    },
+    {
+        id: 5,
+        title: "React",
+        description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+        price: 200,
+        link: "https://open.spotify.com/intl-tr",
+        image: Kurs4
+
+    },
+]
