dfa675c2d1700224b04dfbc6b366735d5e4b80f5
pinarcelik <pcelik157@gmail.com>
2026-03-19 18:58:55 +0300

    listeleme,ekleme ve güncelleme yapıldı

---
+++ 8.api-using/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from "react";
+
+const BASE_URL = "http://localhost:3005";
+function App() {
+
+  const getAllUsers = async () => {
+    const response = await axios.get(BASE_URL + "/users");
+    console.log(response.data);
+  }
+
+  const getUserById = async (userId) => {
+    const response = await axios.get(`${BASE_URL}/users/${userId}`);
+
+    console.log(response.data);
+  }
+
+  const createUser = async (newUser) => {
+    //post veri eklemek iiçin kullanılır
+    const response = await axios.post(`${BASE_URL}/users`, newUser);
+    console.log("response", response.data);
+  }
+
+  const updateUser = async (userId, updateUser) => {
+    await axios.put(`${BASE_URL}/users/${userId}`, updateUser);
+  }
+  useEffect(() => {
+    //getAllUsers();
+    //getUserById(2);
+
+    // const newUser = {
+    //   "username": "Bİlal",
+    //   "passwoed": "3333"
+
+    // }
+    // createUser(newUser);
+
+    updateUser("e4ee", {
+      "username": "yakup",
+      "password": "1234"
+    })
+  }, [])
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
